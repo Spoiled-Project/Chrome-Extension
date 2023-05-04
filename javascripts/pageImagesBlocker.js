b4c3228806b8673c871d9f022b952338d63238e0
Michal Bucks <michall1900@gmail.com>
2023-05-04 23:20:45 +0300

    The problem with the blocked images returned so I fix it again and found what is the problem.

--- javascripts/pageImagesBlocker.js
+++ javascripts/pageImagesBlocker.js
@@ -1,11 +1,12 @@
 (function(){
     const USER_CHOICES = "userChoices";
     const BLOCK_IMAGE_PATH = chrome.runtime.getURL('images/block.jpg');
-    const config = {attributes: true, childList: true, subtree: true, characterData: true}
+
     //const AZURE_URL = "https://spoiledservice.azurewebsites.net"
     const AZURE_URL = "http://127.0.0.1:5000"
-    const PIC_TAGS = 'source, img, image'
-    const POSSIBLE_SRC_ATTR_NAMES = ['src','srcset']
+    const PIC_TAGS = 'source, img'
+    const POSSIBLE_SRC_ATTR_NAMES = ['src','srcset','data-src']
+    const config = {attributes: true, childList: true, subtree: true, characterData: true}
     /**
      * Change the images for the first time - check if there are elements with tags' names that could hold an image
      * and send it to server check.
@@ -63,16 +64,18 @@
         }
 
         function handleResult(res,nodeMap){
+            console.log(res)
             Object.entries(res).forEach(([key,value])=>{
                 if (value){
                     let currentNode = nodeMap.get(key);
-                    POSSIBLE_SRC_ATTR_NAMES.every((attributeName)=>{
-                        let src;
-                        if ((src =currentNode.getAttribute(attributeName)))
-                            (src!==BLOCK_IMAGE_PATH)? currentNode.setAttribute(attributeName,BLOCK_IMAGE_PATH): src=null
-                        console.log(src);
-                        return !src;
-                    })
+                    if(currentNode) {
+                        POSSIBLE_SRC_ATTR_NAMES.every((attributeName) => {
+                            let src;
+                            if ((src = currentNode.getAttribute(attributeName)))
+                                currentNode.setAttribute(attributeName, BLOCK_IMAGE_PATH)
+                            return !src;
+                        })
+                    }
                 }
             })
         }
@@ -80,38 +83,67 @@
         if (!toServer.series.length)
             return;
         let nodeMap = new Map();
-        // temp implementation, will be changed to server's check.
-        elements.forEach((elem)=>{
+        for (let elem of elements){
+            console.log(elem)
             let src;
             if (!POSSIBLE_SRC_ATTR_NAMES.every((attributeName)=>{
+                if (elem.currentSrc)
+                    src = elem.currentSrc
+                else
                     src = elem.getAttribute(attributeName)
-                    return !src;
-                }))
+                if (src === BLOCK_IMAGE_PATH)
+                    src = null;
+                return !src;
+            }))
             {
+                console.log("CATCHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
                 nodeMap.set(src, elem);
                 toServer.images.push(src);
             }
-        })
+        }
         console.log(toServer)
-        if (!!toServer.images.length)
+        if (!!toServer.images.length){
+            console.log("Get")
             getResult(toServer, nodeMap);
+            // [...nodeMap.values()].forEach((currentNode)=>{
+            //     POSSIBLE_SRC_ATTR_NAMES.every((attributeName)=>{
+            //         let src=null;
+            //         if ((src =currentNode.getAttribute(attributeName)))
+            //             currentNode.setAttribute(attributeName,BLOCK_IMAGE_PATH)
+            //         return !src;
+            //     })
+            // })
+
+        }
+
 
     }
 
     //create a mutation observer to handle page changes
     const observe = new MutationObserver((mutations)=>{
-        console.log("mutation")
+        //const sendImages = [];
         mutations.forEach((mutation)=>{
-            mutation.addedNodes.forEach((node) => {
-                if (node.nodeType === Node.ELEMENT_NODE){
-                    const images = node.querySelectorAll(PIC_TAGS)
-                    if (images.length)
-                        getServerResults(images);
-                }
-            })
+                mutation.addedNodes.forEach((node) => {
+                    if (node.nodeType === Node.ELEMENT_NODE){
+                        console.log(node);
+                        console.log("Inside")
+                        const images = node.querySelectorAll(PIC_TAGS)
+                        //const images = Array.from(node.querySelectorAll(PIC_TAGS))
+
+                        if (images.length) {
+                            console.log("Found")
+                            //sendImages.push(...images)
+                            getServerResults(images)
+                        }
+                    }
+                })
         })
+        // if(sendImages.length)
+        //     getServerResults(sendImages);
     })
     changeImages();
     observe.observe(document,config);
-
+    document.addEventListener("DOMContentLoaded",()=>{
+        console.log("LOADEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+    })
 })();
